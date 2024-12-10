c9ff654ed6390a3af318e95a6fbb5d6f577ba740
BigZetena <V.O.Sharipov@gmail.com>
2024-12-10 21:16:00 +0300

    vitya

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { EnterNameForm } from "./components/ui/EnterNameForm/EnterNameForm";
 function App() {
   return (
     <div className="text-red-600 varela">
-      Dima
+      Vitya
       <EnterNameForm label="sanya" placeholder="sosi" id="sanya" />
     </div>
   );
